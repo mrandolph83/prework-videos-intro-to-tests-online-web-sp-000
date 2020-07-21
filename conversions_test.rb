d85b9a14b24441a8be7eb48271b123aa7a9f569e
Matt Smith <mrandolph83@gmail.com>
2020-07-21 16:14:56 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -11,7 +11,7 @@ end
 puts "Given 1, it should return 28.3495"
 answer = ounces_to_grams(1)
 if answer == 28.3495
-  puts "Passed!"
+  puts "Test Passed, yo!"
 else
   puts "Test failed: got #{answer} instead"
 end
