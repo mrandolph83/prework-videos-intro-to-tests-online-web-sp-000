3d42202de93e41ee8f9912cf6068be2f2ce720e2
Matt Smith <mrandolph83@gmail.com>
2020-07-21 16:13:41 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -3,7 +3,7 @@ require_relative './conversions.rb'
 puts "ounces to grams, when given 0 returns 0.0"
 answer = ounces_to_grams(0)
 if answer == 0.0
-  puts "Passed!"
+  puts "Test passed!"
 else
   puts "Test failed: got #{answer} instead"
 end
